2e8c571fff34ea7a6cbc4f4463e2362012032816
Samuel Colvin <s@muelcolvin.com>
2018-05-31 19:17:25 +0100

    simplifying Renderer

--- buildpg/main.py
+++ buildpg/main.py
@@ -24,38 +24,22 @@ class Renderer:
         return self.var_regex.sub(repl, query_template), params
 
     @classmethod
-    def replace(cls, m, *, ctx, add_param):  # noqa: C901 (ignore complexity)
+    def replace(cls, m, *, ctx, add_param):
         var_name, extra_name = m.groups()
         try:
             v = ctx[var_name]
         except KeyError:
             raise BuildError(f'variable "{var_name}" not found in context') from None
 
-        render_gen = None
-        if extra_name:
-            try:
-                render_gen = getattr(v, 'render_' + extra_name)
-            except AttributeError:
-                raise BuildError(f'"{var_name}": extra renderer "{extra_name}" not found')
-        elif isinstance(v, Component):
-            render_gen = v.render
-
-        chunks = []
-
-        def add_chunk(chunk):
-            if isinstance(chunk, Literal):
-                chunks.append(chunk)
-            elif isinstance(chunk, Component):
-                for chunk_ in chunk.render():
-                    add_chunk(chunk_)
-            else:
-                chunks.append(add_param(chunk))
-
         try:
+            render_gen = None
+            if extra_name:
+                render_gen = getattr(v, 'render_' + extra_name)
+            elif isinstance(v, Component):
+                render_gen = v.render
+
             if render_gen:
-                for chunk in render_gen():
-                    add_chunk(chunk)
-                return ''.join(chunks)
+                return ''.join(cls.add_chunk(render_gen, add_param))
             else:
                 return add_param(v)
         except ComponentError as exc:
@@ -64,6 +48,16 @@ class Renderer:
             raise
         except Exception as exc:
             raise BuildError(f'"{var_name}": error building content') from exc
+
+    @classmethod
+    def add_chunk(cls, render_gen, add_param):
+        for chunk in render_gen():
+            if isinstance(chunk, Literal):
+                yield chunk
+            elif isinstance(chunk, Component):
+                yield from cls.add_chunk(chunk.render, add_param)
+            else:
+                yield add_param(chunk)
 
 
 render = Renderer()
